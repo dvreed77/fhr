64de62bda2a74c7705e79aa79cc0c7618273e941
David Reed <david.reed.c@gmail.com>
2016-08-11 19:58:31 -0400

    updated MapTile

--- components/MapTile.js
+++ components/MapTile.js
@@ -5,6 +5,8 @@ import * as L from 'mapbox.js'
 
 import * as d3 from 'd3'
 
+import { Map, Marker, Popup, TileLayer, GeoJson } from 'react-leaflet';
+
 L.mapbox.accessToken = 'pk.eyJ1IjoiZHZyZWVkNzciLCJhIjoibVMzYlVGdyJ9.dYzBhVYkCw1GRZwDxlCsng';
 
 
@@ -13,13 +15,32 @@ console.log('L', L)
 export default class MapTile extends Component {
   componentDidMount() { 
 
-    var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([40, -74.50], 9);
+    // var map = L.mapbox.map(this.refs.map, 'mapbox.streets')
+    // .setView([40, -74.50], 9);
+
+    // L.mapbox.styleLayer('mapbox://styles/dvreed77/cir1ar1fk000lbunsz4rsjc3f').addTo(map);
        
     
   }
 
   render() {
+
+    const position = [51.505, -0.09];
+
+    var divStyle = {
+      width: '270px',
+      height: '175px',
+      float: 'left'
+    };
+
+    const url = 'https://api.mapbox.com/styles/v1/dvreed77/cir1ar1fk000lbunsz4rsjc3f/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZHZyZWVkNzciLCJhIjoibVMzYlVGdyJ9.dYzBhVYkCw1GRZwDxlCsng'
+    
+    const map = (
+    <Map style={divStyle} center={position} zoom={13}>
+      <TileLayer url={url} />
+      <GeoJson data={this.props.route} />
+    </Map>
+    );
 
     console.log(this.props.route)
 
@@ -32,9 +53,7 @@ export default class MapTile extends Component {
     };
 
     return (
-      <div className="MapTile">
-        MapTile
-      </div>
+      <div style={divStyle}>{map}</div>
     )
   }
 }
